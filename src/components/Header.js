045a2e8fbe1edd68ee40a92f02d584d920a49d42
Yvan Sraka <yvan.sraka@gmail.com>
2017-06-24 16:01:41 +0200

    mehdi aime bien

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,8 @@
 import React from 'react';
 
-const sectionStyle = {
-  height: '60px',
-  backgroundImage : 'url("./handiWork.png")',
-  backgroundSize: '80px 60px',
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: '30% 70%',
-  fontSize: '2.5em',
-}
 const Header = () => (
   <header className="hero is-warning" style={{ padding: "2rem" }}>
-    <h1 className="title has-text-centered" style={sectionStyle}>HandiWork</h1>
+    <h1 className="title has-text-centered" style={{ 'line-height': '60px', fontSize: '5em' }}><img src="./handiWork.png" style={{ height: '60px', 'margin-right': '20px', bottom: '-20px' }}/>HandiWork</h1>
   </header>
 );
 
