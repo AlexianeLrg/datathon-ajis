13139563f3592b5dda4b9e7c8d17130fc79a3604
Yvan Sraka <yvan.sraka@gmail.com>
2017-06-24 15:52:24 +0200

    Quick Help

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const ButtonQuestion = (props) => (
-  <p className="control" style={{marginLeft:'400px'}}>
+  <p className="control" style={{ margin: '5px', marginLeft : '400px' }}>
     <Link to={props.url}  className='button is-medium is-warning'>{props.text}</Link>
   </p>
 );
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,10 @@ import React from 'react';
 const sectionStyle = {
   height: '60px',
   backgroundImage : 'url("./handiWork.png")',
-   backgroundSize: '80px 60px',
-   backgroundRepeat: 'no-repeat',
-   backgroundPosition: '30% 70%',
-   fontSize: '2.5em',
-
+  backgroundSize: '80px 60px',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: '30% 70%',
+  fontSize: '2.5em',
 }
 const Header = () => (
   <header className="hero is-warning" style={{ padding: "2rem" }}>
