26e4165d51e1e3db1d7601e9a9059809006add18
Mehdi Druon <mehdidruon@gmail.com>
2017-06-24 13:44:49 +0200

    add some changes

--- src/App.js
+++ src/App.js
@@ -10,16 +10,45 @@ class App extends Component {
         <div>
           <Header />
           <Switch>
-            <Route path='/Q1' render={() => (
-              <Page text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." buttons={[
-                { text: "foo", url: "/Q2" },
-                { text: "dont click me", url: "" },
+            <Route exact path='/' render={() => (
+              <Page title='Bienvenue !' text="Jeune adulte handicapé moteur, vous souhaitez entrer dans la vie professionnelle. Mais vous allez vite découvrir que ce n’est pas un parcours de tout repos. Etes-vous prêt ? " buttons={[
+                { text: "Commencer", url: "/Q1" },
+              ]}/>
+            )}/>/>
+            <Route exact path='/Q1' render={() => (
+              <Page title='1. Le droit commun' text="hjuhguhgruhgruhguhrugugrhugrhughr" buttons={[
+                { text: "dont touch me", url: "/Q2" },
+                { text: "bar", url: "/Q2" },
+                { text: "dont touch me please i'm just a button don't touch me !!!!", url: "/Q2" },
+                { text: "bar", url: "/Q2" },
               ]}/>
             )}/>
-            <Route path='/Q2' render={() => (
-              <Page text="hjuhguhgruhgruhguhrugugrhugrhughr" buttons={[
-                { text: "dont touch me", url: "" },
-                { text: "bar", url: "/Q1" },
+            <Route exact path='/Q2' render={() => (
+              <Page title= '2. Où m’installer ?' text="Vous avez grandi en région lilloise, dans le Nord. Mais vous avez également de la famille en Bretagne. Votre frère habite en Ile-de-France et votre cousine dans la banlieue de Lyon. Toutes les régions ne présentent pas les mêmes opportunités pour les travailleurs handicapés." buttons={[
+                { text: "Je reste dans le Nord", url: "/Q3" },
+                { text: "Direction Paris !", url: "/Q3" },
+                { text: "Je descends à Lyon", url: "/Q3" },
+                { text: "Je préfère la Bretagne", url: "/Q3" },
+              ]}/>
+            )}/>
+            <Route exact path='/Q3' render={() => (
+              <Page title='3. Quel secteur ? ' text="Tous les secteurs professionnels n’embauchent pas de la même façon : certains privilégient l’emploi direct de travailleurs handicapés, d’autres sous-traitent. Le choix d’orientation peut donc être crucial pour votre avenir professionnel. Dans quel domaine souhaitez-vous faire vos études et travailler ?" buttons={[
+                { text: "Le commerce", url: "/Q4" },
+                { text: "L’administration", url: "/Q4" },
+                { text: "L’ingénierie", url: "/Q4" },
+              ]}/>
+            )}/>
+            <Route exact path='/Q4' render={() => (
+              <Page title='4. Comment me former ? ' text="Il faut maintenant choisir le type d’études que vous allez faire. L’Agephip propose aux jeunes handicapés une aide au contrat d’apprentissage. Souhaitez-vous en bénéficier ?" buttons={[
+                { text: "Oui, je veux étudier en apprentissage", url: "/Q5-1" },
+                { text: "Non, je souhaite poursuivre d’autres études", url: "/Q5-2" },
+                { text: "Non, je ne veux pas faire d’études", url: "/Q5-2" },
+              ]}/>
+            )}/>
+            <Route exact path='/Q5-1' render={() => (
+              <Page title='5. L’insertion' text="Votre contrat d’apprentissage vient de se terminer et vous voilà diplômé. Félicitations ! Votre patron propose de vous embaucher. Il peut toucher pour cela une aide à la pérennisation des contrats en alternance. Souhaitez-vous saisir cette opportunité ?" buttons={[
+                { text: "Oui, je veux continuer dans la même entreprise", url: "/Q" },
+                { text: "Non, j’ai envie de créer ma propre entreprise !", url: "/Q2" },
               ]}/>
             )}/>
           </Switch>
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import ButtonQuestion from './Button';
 
+const colorInfo = {
+  backgroundColor: '#900C3F',
+};
+const question = {
+  fontSize: '1.5em',
+}
 const Page = (props) => (
-  <section className="section">
-    <p className="box">{props.text}</p>
-    <div className="field is-grouped">
-      {props.buttons.map(b => (<ButtonQuestion url={b.url} text={b.text}/>))}
-    </div>
-  </section>
+  <div>
+    <section className="section">
+      <h1 className='title has-text-centered'>{props.title}</h1>
+      <p className="box has-text-centered" style={question}>{props.text}</p>
+      <div className="field is-grouped">
+        <ul>
+          <li>
+            {props.buttons.map(b => (<ButtonQuestion url={b.url} text={b.text}/>))}
+          </li>
+        </ul>
+      </div>
+      <article className="message">
+        <div className="message-header has-text-centered" style={colorInfo}>
+          <p>Le saviez-vous?</p>
+          <button className="delete"></button>
+        </div>
+        <div className="message-body">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+        </div>
+      </article>
+    </section>
+  </div>
 );
 
 export default Page;
